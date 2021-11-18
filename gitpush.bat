95748f15713c482005a81b8ae65c45974f09d1b1
UFO360 <2575829997@qq.com>
2021-11-18 10:31:34 +0800

    xz

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,7 @@
 @echo off
 title auto commit
 cd F:\xz\xzstageplus\pic3\pic3
-git config --global http.proxy http://127.0.0.1:10005
+git config --global https.proxy http://127.0.0.1:10005
 git add -A
 git commit -m "xz"
 git push
